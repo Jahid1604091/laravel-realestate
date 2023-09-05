da7f5a8c6b93722750a433d630d2b6d778d5e11e
jahid1604091 <jh409780@gmail.com>
2023-09-05 21:40:25 +0600

    random users created

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,6 +19,11 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'photo' => fake()->imageUrl('60','60'),
+            'phone' => fake()->phoneNumber,
+            'address' => fake()->address,
+            'role' => fake()->randomElement(['admin','user','agent']),
+            'status' => fake()->randomElement(['active','inactive']),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            //admin
+            [
+                'name'=>'Admin',
+                'email'=>'admin@yahoo.com',
+                'password'=>Hash::make('123456'),
+                'role'=>'admin',
+                'status'=>'active'
+            ],
+            //user
+            [
+                'name'=>'User',
+                'email'=>'user@yahoo.com',
+                'password'=>Hash::make('123456'),
+                'role'=>'user',
+                'status'=>'active'
+            ],
+            //agent
+            [
+                'name'=>'Agent',
+                'email'=>'agent@yahoo.com',
+                'password'=>Hash::make('123456'),
+                'role'=>'agent',
+                'status'=>'inactive'
+            ],
+        ]);
+    }
+}
