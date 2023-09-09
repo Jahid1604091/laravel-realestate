78c7f5aa1e67338724f7988a5bccd6b9de34a727
jahid1604091 <jh409780@gmail.com>
2023-09-09 22:02:07 +0600

    auth token and basic operation

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+// Route::group(['middleware'=>'api'], function ($routes) {
+//     Route::post('login', [AuthController::class,'login']);
+// });
+
+//or
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+    Route::get('profile', 'profile');
 });
